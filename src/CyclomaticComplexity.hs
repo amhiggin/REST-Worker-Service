4ffba7ec39c4d4a84f268e4aaf15e3f85631c7f9
Amber <amhiggin@ITSERV.SCSS.TCD.IE>
2017-12-03 16:49:25 +0000

    adding some impl to cyclomatic complexity code

--- src/CyclomaticComplexity.hs
+++ src/CyclomaticComplexity.hs
@@ -1,20 +1,26 @@
--- | Prime factorization
---
--- Written by Dan Weston
--- <http://westondan.blogspot.ie/2007/07/simple-prime-factorization-code.html>
-module CyclomaticComplexity where
+-- Intended to be a file to do all the monkey work with the libraries e.g. Git
+
+module CyclomaticComplexity (
+getCommitList, cloneRepo
+) where
 
 import Control.Monad
 import Git
 import Github.commitSha
 import Github.Commit
 import Argon
+import System.FilePath
+import System.Process
+import System.Directory		-- doesDirectoryExist
+import System.IO
 
 main :: IO ()
 main = do
     getCommits
+    cloneRepo
+    changeCommit
 
-
+-- Function to get a list of commits from github repo
 getCommitList :: IO
 getCommitList = do
     tmp <- commitsFor "argon" "haskell" --FetchAll
@@ -27,3 +33,18 @@ getCommitList = do
 formatCommit :: GitHub.Commit -> String
 formatCommit commit =
    (show $ GitHub.commitSha commit)
+
+
+
+-- Function to clone a git repository given a url
+cloneRepo :: String -> String -> IO ()
+cloneRepo url dir = do
+  exists <- doesDirectoryExist dir		    -- if it exists locally, don't need to clone
+  unless exists $ callProcess "git" ["clone", url]  -- if it doesn't, clone it
+  liftIO $ putStrLn "Finished cloning url"
+  return ()
+
+
+-- Function to change commit version
+changeCommit :: String ()
+changeCommit
