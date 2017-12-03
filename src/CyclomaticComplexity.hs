4c0af203fdcd995221dd28deb3a2ca07745405ea
Amber <amhiggin@ITSERV.SCSS.TCD.IE>
2017-12-03 19:17:59 +0000

    adding pseudocode for outline of required functions
    Outlining the design of the class to calculate the cyclomatic
    complexity, which should be able to do things like cloning from github,
    changing the commit version when required, kicking off a batch
    calculation of cyclomatic complexity which should subsequently kick off
    calculations per-file.
    Need to figure out how we store the results of each calculation, and
    where these methods need to be called in Lib.hs

--- src/CyclomaticComplexity.hs
+++ src/CyclomaticComplexity.hs
@@ -1,7 +1,11 @@
 -- Intended to be a file to do all the monkey work with the libraries e.g. Git
 
 module CyclomaticComplexity (
-getCommitList, cloneRepo
+   getCommits,
+   cloneRepo,
+   changeCommit,
+   calcFileComplexity,
+   calcBatchComplexity
 ) where
 
 import Control.Monad
@@ -14,37 +18,51 @@ import System.Process
 import System.Directory		-- doesDirectoryExist
 import System.IO
 
+
+const repo_url = "http://github.com/rubik/argon"
+
+
 main :: IO ()
 main = do
-    getCommits
     cloneRepo
     changeCommit
 
--- Function to get a list of commits from github repo
-getCommitList :: IO
-getCommitList = do
-    tmp <- commitsFor "argon" "haskell" --FetchAll
-    case tmp of
-        (Left error)    -> putStrLn $ "Error: " ++ (show error)
-        (Right commits) -> do
-            let tmp = intercalate "\n" $ map formatCommit (toList commits)
-            putStrLn tmp
-
-formatCommit :: GitHub.Commit -> String
-formatCommit commit =
-   (show $ GitHub.commitSha commit)
-
-
-
 -- Function to clone a git repository given a url
+-- Returns the dir where the repo has been cloned to
 cloneRepo :: String -> String -> IO ()
 cloneRepo url dir = do
-  exists <- doesDirectoryExist dir		    -- if it exists locally, don't need to clone
+  exists <- doesDirectoryExist (dir ++ url)		    -- if it exists locally, don't need to clone
   unless exists $ callProcess "git" ["clone", url]  -- if it doesn't, clone it
   liftIO $ putStrLn "Finished cloning url"
-  return ()
+  return dir
 
 
--- Function to change commit version
-changeCommit :: String ()
-changeCommit
+-- TODO implement (Function to change commit version)
+-- changeCommit :: String ()
+-- changeCommit
+
+
+getAllCommits :: String -> Process([String])
+getAllCommits url = do
+   dir <- cloneRepo "argon" "/tmp/"
+   -- commitsCmd = " cd "++ folder ++ "&&" ++"/usr/bin/git rev-list HEAD"
+   liftIO $ putStrLn $ "getting commits"  ++  show dir
+   (_, Just hout, _, _) <- liftIO $ createProcess (shell $ commitsCmd ){ std_out = CreatePipe }
+   commits <- liftIO $ hGetContents hout
+   return $ filter (""/=) (splitOn "\n" commits)
+
+
+-- TODO: This function should calculate the complexity of an individual file
+-- calcFileComplexity :: String -> IO ()
+--    do some stuff here
+-- return Double :: value
+
+
+
+-- This function should initiate the calculation of the complexity of a set of files, and return the result
+-- Should be used by the client, since the server will pass a block of files to calculate complexity on
+--calcBatchComplexity
+-- for file : files:
+--  calcFileComplexity
+
+
